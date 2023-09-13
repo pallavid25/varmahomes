9bcdb9a7fbaa875e01c0e083669aeb3acda88226
pallavid25 <pallavid25@gmail.com>
2023-09-13 15:17:38 +0530

    msg

---
+++ index.js
@@ -0,0 +1,16 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "33%";
+    document.getElementById("main").style.marginRight = "250px";
+    document.getElementById("mySidenav").style.backgroundColor = "#ddd";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+    document.body.style.backgroundColor = "white";
+  }
+
+
+  //gallery
+
+  
